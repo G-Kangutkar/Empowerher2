2303dc8f8fa8178c31df5f35e35517dc24ffb9f9
G-Kangutkar <kangutkargauri9205@gmail.com>
2025-11-24 22:51:00 +0530

    done Q20

--- closure/Q20/closure.js
+++ closure/Q20/closure.js
@@ -31,18 +31,29 @@ counter.tripleIncre()// output: 6  ===> 3+3=6 given to next
 
 {
     function createBankccount(){
-        let balance=5000;
+        let balance=0;                //initally balance is 0
+        let transaction = []
         return {
             deposit: function(n){
-                balance = balance+n;
-                console.log(`Deposited ${n}`)
-            },
+                balance = balance+n;             // adding deposit value in balance
+                transaction.push(`Deposited ${balance}`)     /// for transaction history add deposited value to the transcation arr
+                console.log(`Deposited ${n}`)        /// ouputing deposited value
+            },                                      /// balance value is changed . 0 => deposited value
             withdraw: function(n){
-                balance = balance-n;
-                console.log(`withdrown ${n}`)
+                if(balance >=n){                  //checking withdraw value is less than and equal to balance
+                    balance = balance-n;         //if yes withdrawing value from previous balance value i.e deposited value.
+                    transaction.push(`Withdraw ${balance}`)      /// for transaction history add withdraw value to the transcation arr
+                    console.log(`withdrown ${n}`)          
+                }else{
+                    console.log("Insufficient balance")           
+                }
+                
             },
-            checkbal: function(){
-                console.log(balance)
+            checkbal: function(){            /// for checking balance the closure always remebers its parent variables. can access it even the parent function is excuted. 
+                console.log(balance)        /// still remember the parent variables value with what ever updatation happens on it.
+            },
+            transactionHistory : function(){
+                return transaction;             /// returning transcation arr that is parent variable.
             }
         } 
     }
@@ -51,6 +62,7 @@ counter.tripleIncre()// output: 6  ===> 3+3=6 given to next
     account.withdraw(200); // Output: Withdrawn: 200  => 5500-200 = 5300
     account.withdraw(400); // Output: Insufficient balance
     account.checkbal()  // ouput: 4900====> 5000+500-400-200 = 4900
+    account.transactionHistory().forEach(t=>console.log(t))
     console.log(account.balance); // Output: undefined
 
 
