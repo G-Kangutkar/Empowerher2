bec794bc968cfa6d9f7ce3a4bda7fd4e65e8f7e8
G-Kangutkar <kangutkargauri9205@gmail.com>
2025-11-29 22:39:53 +0530

    done Q13

--- eventLoop-timer/Q15/index.js
+++ eventLoop-timer/Q15/index.js
@@ -3,6 +3,17 @@ const ps = require("prompt-sync");
 const prompt = ps();
 let countdown= prompt("Enter the number of seconds to start countdown: ");
 countdown = parseInt(countdown)
+let stop = false;
+let runningTime = countdown;
+
+const checkKey = ()=>{
+     setTimeout(()=>{
+        if(!stop && runningTime >0){
+            checkKey()
+        }
+     },100)
+}
+
 let intervaltime=setInterval(()=>{
     console.log(countdown);
     countdown--;
