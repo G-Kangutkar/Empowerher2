484489afa5ed56474c8be866c6928d920b303f2b
G-Kangutkar <kangutkargauri9205@gmail.com>
2025-12-06 16:06:29 +0530

    Q14

--- localstorage/Q14/index.js
+++ localstorage/Q14/index.js
@@ -1,27 +1,58 @@
-const List = []
-
+const List = [];
+const saved = localStorage.getItem('List');
+if (saved) {
+    try {
+        JSON.parse(saved)
+    }
+    catch {
+        List = [];
+    }
+}
+complete = false;
 // add task
-document.getElementById('addTask').addEventListener('click',()=>{
-    
-    const tasks ={
-        task : document.getElementById('newTask').value,
+document.getElementById('addTask').addEventListener('click', () => {
+
+    const tasks = {
+        task: document.getElementById('newTask').value,
     }
     List.push(tasks);
+    saveData();
     renderCards(List);
 })
 
+function saveData() {
+    localStorage.setItem('List', JSON.stringify(List))
+}
+
+// complete task
+function doneTask(index) {
+    const cards = document.querySelectorAll('.card')
+    const task = cards[index].querySelector('p')
+    task.style.textDecoration = "Line-through"
+    complete = true;
+}
+
+// remove task
+function removeTask(index) {
+    const removed = List.splice(index, 1);
+    localStorage.removeItem('removed');
+    renderCards(List)
+}
+
 //rendering
-const container=document.querySelector(".container")
-function renderCards(data){
-    container.innerHTML='';
-    data.forEach((el,index)=>{
+const container = document.querySelector(".container")
+function renderCards(data) {
+    container.innerHTML = '';
+    data.forEach((el, index) => {
         const card = document.createElement('div');
-        card.className='card';
-        card.innerHTML=`
+        card.className = 'card';
+        card.innerHTML = `
         <p>${el.task}</p>
+        <button id="done" onclick="doneTask(${index})">Done</button>
+        <button id="removeTask" onclick="removeTask(${index})">Remove</button>
 
         `;
-    container.appendChild(card)
+        container.appendChild(card)
     })
 }
 renderCards(List)
