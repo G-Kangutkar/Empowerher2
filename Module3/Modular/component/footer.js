7ffb4f3d4ce0f47715adeba385867d703e6a4b56
G-Kangutkar <kangutkargauri9205@gmail.com>
2025-12-11 21:13:50 +0530

    added styling

--- Module3/Modular/component/footer.js
+++ Module3/Modular/component/footer.js
@@ -1,7 +1,7 @@
 export function footer(){
     return `
     <footer>
-        <p>Contact Us:</p>
+        <h3>Contact Us:</h3>
         <p>Email Id: test@gmail.com</p>
         <p>Copyright@25</p>
     </footer>
