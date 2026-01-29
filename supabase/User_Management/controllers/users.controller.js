062d6a218cdf79c0c7c8db5cc9459461b59b2dcc
G-Kangutkar <kangutkargauri9205@gmail.com>
2026-01-29 18:33:42 +0530

    added hased password logic

--- supabase/User_Management/controllers/users.controller.js
+++ supabase/User_Management/controllers/users.controller.js
@@ -1,10 +1,12 @@
 import supabase from "../config/supabase.config.js";
-
+import bcrypt from "bcrypt";
 export const addUser= async(req,res)=>{
     try {
         const {name,email,password,age,role,created_at} = req.body;
+        const hassedPassword = await bcrypt.hash(password,10)
+
         const {data,error} = await supabase
-        .from('userss').insert([{name,email,password,age,role,created_at}]).select()
+        .from('userss').insert([{name,email,password:hassedPassword,age,role,created_at}]).select("name,email").single()
 
         if(error){
             return res.status(400).json({error:error.message})
@@ -12,7 +14,7 @@ export const addUser= async(req,res)=>{
         res.status(200).json({
             success:true,
             message:'User added successfully!',
-            data:data
+            data:data                
         })
     } catch (error) {
           res.status(500).json({error:error.message})
